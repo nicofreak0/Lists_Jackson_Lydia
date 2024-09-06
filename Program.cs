737c0a3cd7d906f8a456f9b3cc4e8829b5d842f1
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-06 14:53:17 -0500

    Comments

--- Program.cs
+++ Program.cs
@@ -29,60 +29,74 @@ namespace Lists_Jackson_Lydia
                 "Garfield Kart Racing",
             };
 
+            //prints the list of names to console
             foreach(string game in games) 
             { 
                 Console.WriteLine(game);
             }
 
-            Console.WriteLine($"Game in List: {games.Count}");
+            //prints the number of games in the games list
+            Console.WriteLine($"\nGAMES IN LIST: {games.Count}");
 
+            //adds the total number of games in other games and games lists 
             games.AddRange(otherGames);
 
-            Console.WriteLine($"Game in List: {games.Count}");
+            //prints the updated number to console
+            Console.WriteLine($"\nGAMES IN LIST: {games.Count}");
 
+            // checks if halo is in the games list. if its not it adds it
             if (games.Contains("Halo"))
             {
-                Console.WriteLine("MASTER CHIEF is in the house!");
+                Console.WriteLine("\nMASTER CHIEF IS IN THE HOUSE!");
             }
             else
             {
                 games.Add("Halo");
             }
 
+            //sets the value of 6 to an integer
             int myInt = 6;
 
-            if(myInt < games.Count) 
+            //if myInt is less than the count of games remove the game at that index 
+            //otherwise print to console game was not found
+            if (myInt < games.Count) 
             {
                 games.RemoveAt(myInt);
             }
             else
             {
-                Console.WriteLine("Game Not Found.");
+                Console.WriteLine("\nGAME NOT FOUND.");
             }
 
-            Console.WriteLine("All Games in The List:");
+            // prints all the game names in the games list
+            Console.WriteLine("\nALL GAMES IN THE LIST:");
 
             foreach (string game in games)
             {
                 Console.WriteLine(game);
             }
 
+            // sorts the games alphabetically and prints them to console
             games.Sort();
 
-            Console.WriteLine("Sorted Games List:");
+            Console.WriteLine("\nSORTED GAMES LIST:");
 
             foreach (string game in games)
             {
                 Console.WriteLine(game);
             }
 
+            // creates a new string array with the same length as the games list
             string[] newList = new string[games.Count];
 
+            // copy the contents of the games list into the newList array
             games.CopyTo(newList);
 
+            // clears all elements from the games list
             games.Clear();
 
-            Console.WriteLine("New List:");
+            // print the contents of the newList array
+            Console.WriteLine("\nNEW LIST:");
             foreach(string game in newList)
             {
                 Console.WriteLine(game);
